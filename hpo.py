613b181dab558a86ddd066a65c384c5ba2e75858
medhatelhady <medhatelhady382@gmail.com>
2022-08-28 13:41:01 +0200

    edit

--- hpo.py
+++ hpo.py
@@ -220,10 +220,10 @@ def main(args):
 
     print("Initializing Datasets and Dataloaders...")
 
-        # Create training and validation datasets
-        image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'valid']}
-        # Create training and validation dataloaders
-        dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True) for x in ['train', 'valid']}
+    # Create training and validation datasets
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'valid']}
+    # Create training and validation dataloaders
+    dataloaders_dict = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_size, shuffle=True) for x in ['train', 'valid']}
 
 
 
